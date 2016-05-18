fa972969f23cf7fc6d0290d406332d72db2b5680
DanielKong <danielkong1997@gmail.com>
2016-05-18 09:24:05 -0400

    Question 8, not finished

--- 08_strip_comments.py
+++ 08_strip_comments.py
@@ -3,8 +3,25 @@ def solution(string, markers):
 
     :param string: input string
     :type string: str
-    :param markers: 
-    :type markers:
-    :return:
-    :rtype:
+    :param markers: marker that marks where to strip
+    :type markers: list
+    :return: striped string
+    :rtype: str
     """
+    # missing cases when len(marker) > 1
+    # Todo
+    i = 0
+    while i < len(string):
+        if string[i] in markers:
+            if string.find('\n', i) != -1:
+                string = string[:i].rstrip() + string[string.index('\n', i):]
+            else:
+                string = string[:i].rstrip()
+        i += 1
+    return string
+
+if __name__ == "__main__":
+    print(solution("apples, pears # and bananas\ngrapes\nbananas !apples",
+                   ['#', '!']))
+    print("apples, pears\ngrape")
+    print('-' * 10)
