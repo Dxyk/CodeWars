3042b98cb25e2d52a123131f65cc84acc424f6f5
Daniel <danielkong1997@gmail.com>
2016-06-21 13:36:59 -0400

    rename files

--- kyu_4/roman_numeral_decoder_test.py
+++ kyu_4/roman_numeral_decoder_test.py
@@ -1,36 +1,36 @@
 import unittest
-from .roman_numeral_decoder import solution as soln
+from kyu_4.roman_numeral_decoder import solution
 
 
 class TestRomanNumeralDecoder(unittest.TestCase):
     """ Test the Roman Numeral Decoder """
 
     def test_one_numeral(self):
-        self.assertEqual(soln('C'), 100, 'C should be 100')
+        self.assertEqual(solution('C'), 100, 'C should be 100')
 
     def test_same_level_concatenation(self):
-        self.assertEqual(soln('II'), 2, 'II should be 2')
+        self.assertEqual(solution('II'), 2, 'II should be 2')
 
     def test_same_level_concatenation_2(self):
-        self.assertEqual(soln('MM'), 2000, 'CC should be 200')
+        self.assertEqual(solution('MM'), 2000, 'CC should be 200')
 
     def test_two_consecutive_level_concatenation_plus(self):
-        self.assertEqual(soln('VI'), 6, 'VI should be 5 + 1 = 6')
+        self.assertEqual(solution('VI'), 6, 'VI should be 5 + 1 = 6')
 
     def test_two_consecutive_level_concatenation_plus_2(self):
-        self.assertEqual(soln('DC'), 600, 'DC should be 500 + 100 = 600')
+        self.assertEqual(solution('DC'), 600, 'DC should be 500 + 100 = 600')
 
     def test_two_consecutive_level_concatenation_minus(self):
-        self.assertEqual(soln('IV'), 4, 'IV should be 5 - 1 = 6')
+        self.assertEqual(solution('IV'), 4, 'IV should be 5 - 1 = 6')
 
     def test_two_consecutive_level_concatenation_minus_2(self):
-        self.assertEqual(soln('CD'), 400, 'CD should be 500 - 100 = 400')
+        self.assertEqual(solution('CD'), 400, 'CD should be 500 - 100 = 400')
 
     def test_three_level_concatenation(self):
-        self.assertEqual(soln('MIV'), 1004, 'MIV should be 1000 + 5 - 1 = 1004')
+        self.assertEqual(solution('MIV'), 1004, 'MIV should be 1000 + 5 - 1 = 1004')
 
     def test_more_level_concatenation(self):
-        self.assertEqual(soln('MCMXLIII'), 1943,
+        self.assertEqual(solution('MCMXLIII'), 1943,
                          'MCMXLIII should be 1000 + 900 + 43 = 1943')
 
 if __name__ == "__main__":
