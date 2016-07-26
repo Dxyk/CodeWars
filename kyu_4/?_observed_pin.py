c020d5fee5ab2529ed15657be22ffe45888d906d
Daniel <danielkong1997@gmail.com>
2016-07-26 20:55:01 +0900

    observed pin
    itertools.product
    recurssion
    starred

--- kyu_4/?_observed_pin.py
+++ kyu_4/?_observed_pin.py
@@ -1,3 +1,6 @@
+from itertools import product
+
+
 def get_pins(observed):
     """
     return a list of possible pins given an observed pin, where each num could
@@ -33,3 +36,25 @@ def get_pins(observed):
             poss.append(
                 observed[:i] + j + observed[i + 1:])
     return poss
+
+
+ADJACENTS = (
+    '08', '124', '2135', '326', '4157', '52468', '6359', '748', '85790', '968')
+
+
+def soln_get_pins(observed):
+    """
+
+    :param observed: observed pin
+    :type observed: str
+    :return: possible pins
+    :rtype: list[str]
+
+    >>> get_pins('8') == ['5','7','8','9','0']
+    True
+    >>> sorted(get_pins('11'))  == sorted(['11', '22', '44', '12', '21', '14', '41', '24', '42'])
+    True
+    >>> sorted(get_pins('369')) == sorted(["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"])
+    True
+    """
+    return [''.join(p) for p in product(*(ADJACENTS[int(d)] for d in observed))]
