09cd6dabd7507ecfe52abc60b534a0e4094c52e8
Daniel <danielkong1997@gmail.com>
2016-09-02 11:08:09 -0400

    triplet_guessing_string
    Add complete soln

--- kyu_4/triplet_guessing_string.py
+++ kyu_4/triplet_guessing_string.py
@@ -8,7 +8,8 @@ def recover_secret(triplets):
     :return: the secret unknown string
     :rtype: str
 
-    >>> triplets = [['t','u','p'],['w','h','i'],['t','s','u'],['a','t','s'],['h','a','p'],['t','i','s'],['w','h','s']]
+    >>> triplets = [['t','u','p'],['w','h','i'],['t','s','u'],['a','t','s'],\
+    ['h','a','p'],['t','i','s'],['w','h','s']]
     "whatisup"
     """
     r = list(set([i for triplet in triplets for i in triplet]))
@@ -19,7 +20,19 @@ def recover_secret(triplets):
 
 
 def fix(l, a, b):
-    """let l.index(a) < l.index(b)"""
+    # let l.index(a) < l.index(b)
     if l.index(a) > l.index(b):
         l.remove(a)
         l.insert(l.index(b), a)
+
+
+def soln_recover_secret(triplets):
+    letters = list(set([l for t in triplets for l in t]))
+
+    for t in triplets * len(letters):
+        for i in range(len(t) - 1):
+            a, b = letters.index(t[i]), letters.index(t[i + 1])
+            if a > b:
+                letters[b], letters[a] = letters[a], letters[b]
+
+    return ''.join(letters)
