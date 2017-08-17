1fcf006e174a2b514b95559df518ec54b57b89f4
Daniel <danielkong1997@gmail.com>
2017-08-17 17:18:29 +0800

    looking for a benefactor

---
+++ C/kyu_7/looking_for_a_benefactor.c
@@ -0,0 +1,48 @@
+#include <stddef.h>
+#include <stdio.h>
+
+/*
+ * The accounts of the "Fat to Fit Club (FFC)" association are supervised by
+John as a volunteered accountant. The association is funded through financial
+donations from generous benefactors. John has a list of the first n donations:
+[14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should
+give to the association so that the average of the first n + 1 donations should
+reach an average of 30. After doing the math he found 149. He thinks that he
+made a mistake. Could you help him?
+
+if dons = [14, 30, 5, 7, 9, 11, 15] then new_avg(dons, 30) --> 149
+
+The function new_avg(arr, navg) should return the expected donation (rounded up
+to the next integer) that will permit to reach the average navg.
+
+Should the last donation be a non positive number (<= 0) John wants us to throw
+an error (return Nothing in Haskell, return None in F# and Ocaml, return -1 in
+C) so that he clearly sees that his expectations are not great enough.
+
+Notes:
+
+all donations are numbers, arr can be empty.
+See examples below to see which error is to be thrown.
+new_avg([14, 30, 5, 7, 9, 11, 15], 92) should return 645
+new_avg([14, 30, 5, 7, 9, 11, 15], 2) should raise an error (ValueError or
+invalid_argument)
+ */
+
+long long newAvg(double *arr, size_t szArray, double navg) {
+	double exp_total = navg * (szArray + 1);
+	double current_total = 0;
+	for (int i = 0; i < szArray; i++) {
+		current_total += arr[i];
+	}
+	double diff = exp_total - current_total;
+	if (diff < 0) {
+		return -1;
+	}
+	return ((int)(diff * 10) % 10 > 5) ? diff + 1 : diff;
+}
+
+int main(int argc, char const *argv[]) {
+	double arr[7] = {14, 30, 5, 7, 9, 11, 15};
+	printf("%llu\n", newAvg(arr, 7, 92));
+	return 0;
+}
